ecdc24b0aa905208089ddffc37e972bc4d90a71f
PohPrinceton <niceprinceton@gmail.com>
2022-05-26 05:51:17 +0800

    wk1 finished guided tutorial

--- wk1/Assets/Scripts/PlayerController.cs
+++ wk1/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,13 @@
+using System.Timers;
+using System.Threading;
+using System.Globalization;
+using System;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,6 +18,10 @@ public class PlayerController : MonoBehaviour
     public float upSpeed;
     private SpriteRenderer marioSprite;
     private bool faceRightState = true;
+    public Transform enemyLocation;
+    public Text scoreText;
+    private int score = 0;
+    private bool countScoreState = false;
     
 
     
@@ -38,6 +49,17 @@ public class PlayerController : MonoBehaviour
             faceRightState = true;
             marioSprite.flipX = false;
         }
+
+        // when jumping, and Gomba is near Mario and we haven't registered our score
+        if (!onGroundState && countScoreState)
+        {
+            if (Mathf.Abs(transform.position.x - enemyLocation.position.x) < 0.5f)
+            {
+                countScoreState = false;
+                score++;
+                Debug.Log(score);
+            }
+        }
     }
 
     // FixedUpdate may be called once per frame. See documentation for details.
@@ -58,12 +80,22 @@ public class PlayerController : MonoBehaviour
             marioBody.AddForce(Vector2.up * upSpeed, ForceMode2D.Impulse);
             onGroundState = false;
         }
+        if (Input.GetKeyDown("space") && onGroundState)
+        {
+            marioBody.AddForce(Vector2.up * upSpeed, ForceMode2D.Impulse);
+            onGroundState = false;
+            countScoreState = true; //check if Gomba is underneath
+        }
     }
 
-    // called when the cube hits the floor
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.CompareTag("Ground")) onGroundState = true;
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            onGroundState = true; // back on ground
+            countScoreState = false; // reset score state
+            scoreText.text = "Score: " + score.ToString();
+        };
     }
 
     void OnTriggerEnter2D(Collider2D other)
