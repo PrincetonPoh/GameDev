ebd90a9485433d4594897e2f8bead3cba9d2aef2
PohPrinceton <niceprinceton@gmail.com>
2022-05-26 05:27:19 +0800

    wk1 can flip sprite

--- wk1/Assets/Scripts/PlayerController.cs
+++ wk1/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float maxSpeed = 10;
     private Rigidbody2D marioBody;
+    private bool onGroundState = true;
+    public float upSpeed;
+    private SpriteRenderer marioSprite;
+    private bool faceRightState = true;
+    
 
     
     // Start is called before the first frame update
@@ -14,18 +20,49 @@ public class PlayerController : MonoBehaviour
         // Set to be 30 FPS
         Application.targetFrameRate =  30;
         marioBody = GetComponent<Rigidbody2D>();
+        
+        // initialise Mario Sprite
+        marioSprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // toggle state
+        if (Input.GetKeyDown("a") && faceRightState){
+            faceRightState = false;
+            marioSprite.flipX = true;
+        }
+
+        if (Input.GetKeyDown("d") && !faceRightState){
+            faceRightState = true;
+            marioSprite.flipX = false;
+        }
     }
 
-    void  FixedUpdate()
-    {
+    // FixedUpdate may be called once per frame. See documentation for details.
+    void FixedUpdate()
+    {  
+        // dynamic rigidbody
         float moveHorizontal = Input.GetAxis("Horizontal");
-        Vector2 movement = new Vector2(moveHorizontal, 0);
-        marioBody.AddForce(movement * speed);
+        if (Mathf.Abs(moveHorizontal) > 0){
+            Vector2 movement = new Vector2(moveHorizontal, 0);
+            if (marioBody.velocity.magnitude < maxSpeed)
+                    marioBody.AddForce(movement * speed);
+        }
+        if (Input.GetKeyUp("a") || Input.GetKeyUp("d")){
+            // stop
+            marioBody.velocity = Vector2.zero;
+        }
+        if (Input.GetKeyDown("space") && onGroundState){
+            marioBody.AddForce(Vector2.up * upSpeed, ForceMode2D.Impulse);
+            onGroundState = false;
+        }
+    }
+
+    // called when the cube hits the floor
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Ground")) onGroundState = true;
     }
 }
