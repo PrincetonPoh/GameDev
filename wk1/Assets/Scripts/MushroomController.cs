a5a2eb6fb10b8e5ef427dd526ea3e9ac33fe4104
PohPrinceton <niceprinceton@gmail.com>
2022-06-02 05:05:23 +0800

    bouncing mushroom

---
+++ wk1/Assets/Scripts/MushroomController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class MushroomController : MonoBehaviour
+{
+    public float speed;
+    private bool collision;
+    private bool start = false;
+    private bool movingRight = true;
+    private Rigidbody2D mushroomBody;
+    private bool onGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mushroomBody = GetComponent<Rigidbody2D>();
+        mushroomBody.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+        start = true;
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (!start || collision) return;
+        var velocity = mushroomBody.velocity;
+        velocity = movingRight ? new Vector2(speed, velocity.y) : new Vector2(-speed, velocity.y);
+        print("Deeznuts");
+        mushroomBody.velocity = velocity;
+    }
+
+    void onBecameInvisible(){
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+        if (!collision && other.gameObject.CompareTag("Player")){
+            print("HIT");
+            collision = true;
+            mushroomBody.velocity = Vector2.zero;
+            return;
+        }
+        var dir = other.GetContact(0).normal;
+        if (dir==Vector2.up) return;
+        if (other.gameObject.CompareTag("Obstacle")){
+            movingRight = !movingRight;
+        }
+    }
+}
